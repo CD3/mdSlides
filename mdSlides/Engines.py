28ef9339864d11a815db202b7435e4343d1ed835
CD Clark III <clifton.clark@gmail.com>
2020-08-13 08:13:17 -0500

    feat: added support for pre and post processing

--- mdSlides/Engines.py
+++ mdSlides/Engines.py
@@ -1,12 +1,60 @@
 import subprocess, pathlib, os, shutil, urllib
 from . import Utils
 
-class Engine:
-  def preprocess(self,input):
-    pass
+def is_exe(path):
+  if path.is_file():
+    if os.access(str(path),os.X_OK):
+      return True
+  return False
 
-  def postprocess(self,input):
-    pass
+class Engine:
+  '''
+  A base class that implements common code for all engines. To add a new engine, one just needs to derive
+  from this class 
+  '''
+
+  def __init__(self, preprocess_script_name=None, postprocess_script_name=None):
+
+    self.preprocess_script = self.make_path("preprocess" if preprocess_script_name is None else preprocess_script_name)
+    self.postprocess_script = self.make_path("postprocess" if postprocess_script_name is None else postprocess_script_name)
+
+  def preprocess(self,input):
+    if is_exe(self.preprocess_script):
+      input_dir = input.parent
+      processed_input = input_dir/f"{str(input.stem)}-process.md"
+
+      print(f"Pre-processing: {input} -> {processed_input}")
+
+      cmd = ["./"+str(self.preprocess_script),str(input),str(processed_input)]
+      self.run_cmd(cmd)
+
+      return processed_input
+    
+    return input
+
+  def postprocess(self,output):
+    if is_exe(self.postprocess_script):
+      output_dir = output.parent
+
+      print(f"Post-processing: {output}")
+
+      cmd = ["./"+str(self.postprocess_script),str(output)]
+      self.run_cmd(cmd)
+
+  def build(self,input,output=None):
+    '''Builds a presentation from an input markdown file. First calls .preprocess(...) method,
+    then ._build(...) method (which should be impemented by derived class, aad finally calls
+    .postprocess(...) method.
+    '''
+    input = self.make_path(input)
+    if output is None:
+      output_dir = self.setup_output_dir(input,output)
+      output = output_dir/self._get_default_output_filename(input)
+
+    input = self.preprocess(input)
+    print(f"Generating slides: {input} -> {output}")
+    output = self._build(input,output)
+    self.postprocess(output)
 
   def make_path(self,file):
     return pathlib.Path(file)
@@ -27,11 +75,13 @@ class Engine:
 
     return output_path
 
-  def run_cmd(self,cmd,desc=None):
+  def run_cmd(self,cmd,desc=None,verbose=0):
    if desc is None:
      desc = "running:" + " ".join(cmd)
    else:
-     print(desc)
+     if verbose > 0:
+       print(desc)
+
    result = subprocess.run(cmd, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
    if result.returncode != 0:
      print(f"There was an error {desc}")
@@ -65,12 +115,13 @@ class Engine:
 
 class PandocSlidy(Engine):
 
-  def build(self,input,output=None):
+  def _get_default_output_filename(self,input):
+    '''return default path to output file for a given input file.'''
+    return "index.html"
 
-    input = super().make_path(input)
-    output_dir = super().setup_output_dir(input,output)
-    output = output_dir / "index.html"
-    print(f"{input} -> {output}")
+  def _build(self,input,output):
+
+    output_dir = output.parent
 
     if not (output_dir/"data").exists():
       super().run_cmd(['git','clone', 'https://github.com/slideshow-templates/slideshow-slidy.git',str(output.parent/"data")],"fetching slidy data files")
@@ -100,17 +151,17 @@ class PandocSlidy(Engine):
 
     super().copy_images_to_output(output)
 
+    return output
+
 
   
 class PandocPowerPoint(Engine):
 
-  def build(self,input,output=None):
+  def _get_default_output_filename(self,input):
+    '''return default path to output file for a given input file.'''
+    return str(input.stem)+".pptx"
 
-    input = super().make_path(input)
-    output_dir = super().setup_output_dir(input,output)
-    output = output_dir / (str(input.stem)+".pptx")
-    print(f"{input} -> {output}")
-
+  def _build(self,input,output):
     template_file = pathlib.Path("mdSlides-template.pptx")
 
     cmd = list()
@@ -123,3 +174,5 @@ class PandocPowerPoint(Engine):
       cmd.append(str(template_file))
 
     super().run_cmd(cmd,"building the slides.")
+
+    return output
---
+++ testing/run-tests.t
@@ -0,0 +1,33 @@
+  $ cp -r ${TESTDIR}/Simple ./
+  $ ls ./Simple
+  simple.md
+  $ mdSlides ./Simple/simple.md
+  Generating slides: Simple/simple.md -> Simple/simple/index.html
+  $ ls ./Simple
+  simple
+  simple.md
+  $ ls ./Simple/simple
+  data
+  index.html
+
+
+  $ cp -r ${TESTDIR}/Processing ./
+  $ cd ./Processing
+  $ ls
+  postprocess
+  preprocess
+  slides.md
+  $ mdSlides ./slides.md
+  Pre-processing: slides.md -> slides-process.md
+  Generating slides: slides-process.md -> slides/index.html
+  Post-processing: slides/index.html
+  $ ls 
+  postprocess
+  preprocess
+  slides
+  slides-process.md
+  slides.md
+  $ ls ./slides
+  data
+  index.html
+  $ cd ..
