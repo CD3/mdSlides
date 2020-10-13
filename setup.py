13cc96dbdd37943977dc84826f0f46400a44fb4b
CD Clark III <clifton.clark@gmail.com>
2020-10-13 14:41:03 -0500

    housekeeping: updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='mdSlides',
     license="MIT",
-    version="0.0.3",
+    version="0.0.4",
     description='A tool for generating slides from markdown files using different backends.',
     url='https://github.com/CD3/mdSlides',
     author='C.D. Clark III',
